3957a908eb920e87d414ddec344bdd253e6ab918
yidit <zcf124578@163.com>
2023-08-29 18:59:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -13,6 +13,9 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
+# 更改默认 Shell 为 bash
+sed -i 's/\/bin\/ash/\/bin\/bash/g' package/base-files/files/etc/passwd
+
 # TTYD 自动登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
